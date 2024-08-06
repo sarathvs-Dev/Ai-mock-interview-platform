b4a798937fd260dff4a71acfe0d04616f8b72a25
sarathvs-Dev <sarathvs0880@gmail.com>
2024-08-06 13:49:32 +0530

    interview platform first committing after completeing start interview modal

---
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React, { useState } from "react";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+
+function AddNewInterview() {
+    const [opemDailog,setOpenDialog]=useState(false)
+  return (
+    <div>
+      <div className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+      onClick={()=>setOpenDialog(true)}>
+        <h2 className="font-bold text-lg text-center">+ Add New</h2>
+      </div>
+      <Dialog open={opemDailog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+
+              <div className="flex gap-5 justify-end">
+                <Button variant="ghost" onClick={()=>setOpenDialog(false)}>Cancel</Button>
+                <Button>Start Interview</Button>
+              </div>
+            </DialogDescription>
+        
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddNewInterview;
