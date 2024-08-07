0ad6d9e4ec1ca8e6c4c9d0f3d02de2efd759c9a9
sarathvs-Dev <sarathvs0880@gmail.com>
2024-08-07 18:55:31 +0530

    after gemini ai implementation

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,39 +1,151 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { chatSession } from "@/utils/GeminiAiModal";
+import { LoaderCircle } from "lucide-react";
+import { db } from "@/utils/db";
+import { MockInterview } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import moment from 'moment'
+
 
 function AddNewInterview() {
-    const [opemDailog,setOpenDialog]=useState(false)
+  const [opemDailog, setOpenDialog] = useState(false);
+  const [jobPosition, setJobPosition] = useState();
+  const [jobDesc, setJobDesc] = useState();
+  const [jobExperince, setExperince] = useState();
+  const [loading,setLoading]=useState(false);
+  const [jsonResponse,setJsonResponse]=useState([]);
+  const {user}=useUser()
+
+  const onSubmit = async (e) => {
+    setLoading(true)
+    e.preventDefault();
+    console.log(jobDesc, jobExperince, jobPosition);
+
+    const InputPrompt =
+      "job position:" +
+      jobPosition +
+      ", job descripton:" +
+      jobDesc +
+      "and years of experinece " +
+      jobExperince +
+      " years\n\n\ndepand on this information please give me " +
+      process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT +
+      " questions with answerd in json fromat";
+
+      const result= await chatSession.sendMessage(InputPrompt)
+
+      const MockResponse=(result.response.text()).replace('```json','').replace('```','')
+      console.log(JSON.parse(MockResponse))
+      setJsonResponse(MockResponse)
+if(MockResponse){
+
+
+      const  resp=await db.insert(MockInterview).values({
+        mockId:uuidv4(),
+        jsonMockResp:MockResponse,
+        jobPosition:jobPosition,
+        jobDesc:jobDesc,
+        jobExperience:jobExperince,
+        createdBy:user?.primaryEmailAddress?.emailAddress,
+        createdAt: moment().format('DD-MM-YYYY')
+
+      }).returning({mockId:MockInterview.mockId})
+
+
+      console.log(resp)
+    }
+    else{
+      console.log("ERROR");
+      
+    }
+      setLoading(false)
+
+  }
+
+
   return (
     <div>
-      <div className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
-      onClick={()=>setOpenDialog(true)}>
+      <div
+        className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+        onClick={() => setOpenDialog(true)}
+      >
         <h2 className="font-bold text-lg text-center">+ Add New</h2>
       </div>
       <Dialog open={opemDailog}>
-        <DialogContent>
+        <DialogContent className="max-w-2xl">
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className="text-2xl">
+              Tell us more about your job interviewing
+            </DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <form onSubmit={onSubmit}>
+                <div>
+                  {/* <h2 className="font-bold text-2xl"></h2> */}
+                  <h2>
+                    Add deatils about your job position/role, job description
+                    and years of experience
+                  </h2>
 
-              <div className="flex gap-5 justify-end">
-                <Button variant="ghost" onClick={()=>setOpenDialog(false)}>Cancel</Button>
-                <Button>Start Interview</Button>
-              </div>
+                  <div className="mt-7 my-3">
+                    <label>Job Role/Job Position</label>
+                    <Input
+                      placeholder="Ex. Full stack Developer"
+                      required
+                      onChange={(event) => setJobPosition(event.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label>Job Description/Tech Stack(In Short)</label>
+                    <Textarea
+                      placeholder="Ex. React,Angular,Node,Python,Mysql etc"
+                      required
+                      onChange={(event) => setJobDesc(event.target.value)}
+                    />
+                  </div>
+                  <div className="mt-7 my-3">
+                    <label>Years of Experience</label>
+                    <Input
+                      placeholder="Ex. 5"
+                      type="number"
+                      required
+                      max="30"
+                      min="0"
+                      onChange={(event) => setExperince(event.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-5 justify-end">
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    onClick={() => setOpenDialog(false)}
+                  >
+                    Cancel
+                  </Button>
+                  <Button disabled={loading} type="submit">
+                    {loading?
+                    <>
+                    <LoaderCircle className="animate-spin"/>'generating'</>:'Start Interview'}
+                    </Button>
+                </div>
+              </form>
             </DialogDescription>
-        
           </DialogHeader>
         </DialogContent>
       </Dialog>
